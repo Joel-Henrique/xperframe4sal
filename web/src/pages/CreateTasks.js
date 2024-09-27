69db46346c114948e85df50e4e55e0ff1998e7c5
Joel Henrique <104386844+Joel-Henrique@users.noreply.github.com>
2024-09-27 10:41:30 -0300

    Update CreateTasks.js

--- web/src/pages/CreateTasks.js
+++ web/src/pages/CreateTasks.js
@@ -10,11 +10,9 @@ const CreateTasks = () => {
     return (
         <>
             <Typography variant="h6" gutterBottom sx={{ fontSize: { xs: '1.0rem', sm: '1.2rem', marginBottom: 30 } }}>
-                Assista a seguir um vídeo com instruções de utilização deste sistema:
+                Criação de tarefas
             </Typography>
-            <Box component='div' sx={{ display: 'flex', justifyContent: 'center' }}>
-                <Box sx={{ width: { sm: 654, xs: 360 }, height: { sm: 375, xs: 205 } }} component='iframe' src="https://www.youtube.com/embed/Q4C4ees0VLQ?rel=0&si=6_lCAUI5nbnHa_yJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" allowFullScreen />
-            </Box>
+
         </>
     );
 };
