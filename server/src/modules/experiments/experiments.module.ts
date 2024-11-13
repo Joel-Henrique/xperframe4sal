084aa36edfb9834a84d3a03713a6ef87b85a3052
Joel Henrique <joel.henrique@estudante.ufjf.br>
2024-11-13 16:15:17 -0300

    linka usertaskprops com user-task

--- server/src/modules/experiments/experiments.module.ts
+++ server/src/modules/experiments/experiments.module.ts
@@ -3,13 +3,15 @@ import { ExperimentsService } from './experiments.service';
 import { ExperimentsController } from './experiments.controller';
 import { ExperimentSchema } from '../../model/experiment.entity';
 import { UserExperimentSchema } from '../../model/user-experiment.entity';
+import { UserTaskSchema } from '../../model/user-task.entity';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: "Experiments", schema: ExperimentSchema },
-      { name: "UserExperiment", schema: UserExperimentSchema }
+      { name: "UserExperiment", schema: UserExperimentSchema },
+      { name: "UserTask", schema: UserTaskSchema }
     ]),
   ],
   controllers: [ExperimentsController],
--- server/src/modules/experiments/experiments.service.ts
+++ server/src/modules/experiments/experiments.service.ts
@@ -3,39 +3,68 @@ import { Experiment } from '../../model/experiment.entity';
 import { UserExperiment } from '../../model/user-experiment.entity'
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UserTask } from 'src/model/user-task.entity';
+
 @Injectable()
 export class ExperimentsService {
   constructor(
     @InjectModel('Experiments')
     private readonly _experimentModel: Model<Experiment>,
+
     @InjectModel('UserExperiment')
-    private readonly _userExperimentModel: Model<UserExperiment>
+    private readonly _userExperimentModel: Model<UserExperiment>,
+
+    @InjectModel('UserTask')
+    private readonly _userTaskModel: Model<UserTask>
   ) { }
 
   async create(experiment: Experiment): Promise<Experiment> {
-    const userIds = []
-
+    const userIds = [];
+    const taskIds = [];
     
-
     Object.keys(experiment.userProps).forEach((userId) => {
       const userProps = experiment.userProps[userId];
-      userIds.push(userProps)
+      userIds.push(userProps);
     });
+
+    Object.keys(experiment.tasksProps).forEach((taskId) => {
+      const taskProps = experiment.tasksProps[taskId];
+      taskIds.push(taskProps);
+    });
+
     const newExperiment = new this._experimentModel(experiment);
-    const _experiment = await newExperiment.save()
-    for(let i = 0; i < userIds.length; i++) {
-      const userId = userIds[i]
+    const _experiment = await newExperiment.save();
+
+    // Salva UserExperiments
+    for (let i = 0; i < userIds.length; i++) {
+      const userId = userIds[i];
       const userExperiment = new this._userExperimentModel({
         userId: userId,
-        experimentId: _experiment._id, 
+        experimentId: _experiment._id,
       });
 
       await userExperiment.save();
     }
 
-    
-    return _experiment
+    // Salva UserTasks
+    for (let i = 0; i < taskIds.length; i++) {
+      const taskId = taskIds[i];
+      for (let i = 0; i < userIds.length; i++){
+        const userId = userIds[i];
+        const userTask = new this._userTaskModel({
+          userId: userId,
+          taskId: taskId,
+      });
+
+      await userTask.save();
+    }
+    }
+
+    return _experiment;
   }
+
+
+  
 
   async findAll(): Promise<Experiment[]> {
     return await this._experimentModel.find().exec();
