115ae960338cb8a96eba5915940905751055f980
Alfredo Lucas <alfredoliu14@live.com>
2024-11-25 13:22:34 -0300

    finaliza service e controller do user2

--- server/src/modules/user2/user2.controller.ts
+++ server/src/modules/user2/user2.controller.ts
@@ -1,11 +1,64 @@
-import {Controller, Get, NotFoundException, Query} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import {User2Service} from './user2.service';
-import {GetUserDto} from 'src/model/user.dto';
+import {
+  ForgotPasswordDto,
+  GetRecoveryPasswordDto,
+  GetUserDto,
+  ResetPasswordDto,
+} from 'src/model/user.dto';
+import {User} from './entity/user.entity';
 
 @Controller('user2')
 export class User2Controller {
   constructor(private readonly _userService: User2Service) {}
 
+  @Post('forgot-password')
+  async forgotPassword(
+    @Body() forgotPasswordDto: ForgotPasswordDto,
+  ): Promise<void> {
+    try {
+      await this._userService.forgotPassword(forgotPasswordDto);
+    } catch (error) {
+      throw error;
+    }
+  }
+  @Post('reset-password')
+  async resetPassword(
+    @Body() resetPasswordDto: ResetPasswordDto,
+  ): Promise<void> {
+    try {
+      await this._userService.resetPassword(resetPasswordDto);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+  @Post()
+  async create(@Body() user: User): Promise<GetUserDto> {
+    try {
+      user.name = user.name.trim();
+      user.lastName = user.lastName.trim();
+      user.email = user.email.trim();
+      const userDto = await this._userService.create(user);
+      return {
+        id: userDto._id,
+        name: userDto.name,
+        lastName: userDto.lastName,
+        email: userDto.email,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
   //@UseGuards(AuthGuard('jwt'))
   @Get()
   async findAll(
@@ -45,5 +98,38 @@ export class User2Controller {
     } catch (error) {
       throw error;
     }
+  }
+  //@UseGuards(AuthGuard('jwt'))
+  @Get(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() user: User,
+  ): Promise<GetUserDto> {
+    //user.lastChangedAt = new Date();
+    const userDto = await this._userService.update(id, user);
+    return {
+      id: userDto._id,
+      name: userDto.name,
+      lastName: userDto.lastName,
+      email: userDto.email,
+    };
+  }
+  //@UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return await this._userService.remove(id);
+  }
+  //@UseGuards(AuthGuard('jwt'))
+  @Patch()
+  async addChangesPasswordToken(
+    @Query('email') email: string,
+  ): Promise<GetRecoveryPasswordDto> {
+    const user = await this._userService.addChangesPasswordToken(email);
+    return {
+      id: user._id,
+      recoveryPasswordToken: user.recoveryPasswordToken,
+      recoveryPasswordTokenExpirationDate:
+        user.recoveryPasswordTokenExpirationDate,
+    };
   }
 }
--- server/src/modules/user2/user2.service.ts
+++ server/src/modules/user2/user2.service.ts
@@ -1,4 +1,5 @@
 import {
+  HttpException,
   Injectable,
   NotFoundException,
   UnauthorizedException,
@@ -7,13 +8,79 @@ import {InjectRepository} from '@nestjs/typeorm';
 import {User} from './entity/user.entity';
 import {Repository} from 'typeorm';
 import * as bcrypt from 'bcrypt';
-//import * as crypto from 'crypto';
+import {ForgotPasswordDto, ResetPasswordDto} from 'src/model/user.dto';
+import * as crypto from 'crypto';
+import {MailerService} from '@nestjs-modules/mailer';
+
+const HOUR_1 = 3600000;
 @Injectable()
 export class User2Service {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly mailerService: MailerService,
   ) {}
+
+  async forgotPassword(forgotPasswordDto: ForgotPasswordDto): Promise<void> {
+    const {email} = forgotPasswordDto;
+    let user = null;
+    try {
+      user = await this.findOneByEmail(email);
+    } catch (error) {
+      throw error;
+    }
+    user.recoveryPasswordToken = crypto.randomBytes(20).toString('hex');
+    user.recoveryPasswordTokenExpirationDate = new Date(Date.now() + HOUR_1);
+    try {
+      await this.userRepository.update(user._id, {
+        recoveryPasswordToken: user.recoveryPasswordToken,
+        recoveryPasswordTokenExpirationDate:
+          user.recoveryPasswordTokenExpirationDate,
+      });
+    } catch (error) {
+      throw new HttpException(
+        `Ocorreu um erro inesperado tente novamente`,
+        500,
+      );
+    }
+    try {
+      await this.sendResetPasswordEmail(user.email, user.recoveryPasswordToken);
+    } catch (error) {
+      throw new HttpException(
+        `Ocorreu um errro inesperado tente novamente`,
+        500,
+      );
+    }
+  }
+  async resetPassword(resetPasswordDto: ResetPasswordDto): Promise<void> {
+    try {
+      const user = await this.findOneByEmail(resetPasswordDto.email);
+      if (
+        user.recoveryPasswordToken &&
+        user.recoveryPasswordToken !== resetPasswordDto.token
+      ) {
+        throw new Error('Token inválido');
+      }
+      if (user.recoveryPasswordTokenExpirationDate < new Date()) {
+        throw new Error('Token expirado');
+      }
+      user.password = resetPasswordDto.password;
+      user.recoveryPasswordToken = null;
+      user.recoveryPasswordTokenExpirationDate = null;
+      await this.update(user._id, user);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async create(user: User): Promise<User> {
+    try {
+      user.password = await bcrypt.hash(user.password, 10);
+      const userSaved = await this.userRepository.save(user);
+      return userSaved;
+    } catch (error) {
+      throw error;
+    }
+  }
   async findAll(): Promise<User[]> {
     try {
       return await this.userRepository.find();
@@ -48,7 +115,15 @@ export class User2Service {
       throw error;
     }
   }
-
+  async addChangesPasswordToken(email: string): Promise<User> {
+    const user = await this.findOneByEmail(email);
+    const token = crypto.randomBytes(32).toString('hex');
+    const expirationDate = new Date();
+    expirationDate.setMinutes(expirationDate.getMinutes() + 30);
+    user.recoveryPasswordToken = token;
+    user.recoveryPasswordTokenExpirationDate = expirationDate;
+    return await this.update(user._id, user);
+  }
   async findByEmailAndPassword({email, password}): Promise<User> {
     try {
       const user = await this.findOneByEmail(email);
@@ -58,6 +133,52 @@ export class User2Service {
       throw new UnauthorizedException();
     } catch (error) {
       throw new Error(error.message);
+    }
+  }
+  async update(id: string, user: User): Promise<User> {
+    try {
+      if (user.password?.length > 0) {
+        user.password = await bcrypt.hash(user.password, 10);
+      } else {
+        user.password = undefined;
+      }
+      await this.userRepository.update(id, user);
+      const result = await this.findOne(id);
+      return result;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+  async remove(id: string) {
+    return await this.userRepository.delete(id);
+  }
+  async removeAll() {
+    try {
+      const result = await this.userRepository
+        .createQueryBuilder()
+        .delete()
+        .from(User)
+        .execute();
+
+      return {n: result.affected || 0};
+    } catch (error) {
+      throw error;
+    }
+  }
+  private async sendResetPasswordEmail(
+    email: string,
+    token: string,
+  ): Promise<void> {
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        from: process.env.MY_GMAIL_EMAIL,
+        subject: 'Alteração de Senha plataforma Buscando e Aprendendo',
+        text: `Por favor, acesse o link a seguir para restaurar sua senha: https://buscandoeaprendendo.onrender.com/reset-password?token=${token}&email=${email}`,
+        html: `Por favor, acesse o link a seguir para restaurar sua senha: <a href='https://buscandoeaprendendo.onrender.com/reset-password?token=${token}&email=${email}'>https://buscandoeaprendendo.onrender.com/reset-password?token=${token}</a>`,
+      });
+    } catch (error) {
+      throw error;
     }
   }
 }
