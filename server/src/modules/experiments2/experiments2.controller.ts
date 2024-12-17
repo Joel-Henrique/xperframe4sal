c788a44e97f4161956f5d71728322271b9d4541c
Alfredo Lucas <alfredoliu14@live.com>
2024-12-17 18:33:29 -0300

    Adds createDTO as a parameter in the create method of the Experiment controller

--- server/src/modules/experiments2/experiments2.controller.ts
+++ server/src/modules/experiments2/experiments2.controller.ts
@@ -1,14 +1,27 @@
-import {Body, Controller, Delete, Get, Param, Patch} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import {Experiments2Service} from './experiments2.service';
 import {Experiment} from './entity/experiment.entity';
+import {CreateExperimentDto} from './dto/create-experiment.dto';
 
 @Controller('experiments2')
 export class Experiments2Controller {
   constructor(private readonly experimentService: Experiments2Service) {}
 
   //TODO
-  //@Post()
-  //async create()
+  @Post()
+  async create(
+    @Body() createExperimentDto: CreateExperimentDto,
+  ): Promise<Experiment> {
+    return await this.experimentService.create(createExperimentDto);
+  }
 
   //UseGuards(AuthGuard('jwt'))
   @Get()
