f70983fb5ab5e84e76d6a9f69eabbb112402c527
Alfredo Lucas <alfredoliu14@live.com>
2025-02-09 15:14:20 -0300

    Fix dependency injection error  in SurveyAnswer2Module

--- server/src/modules/survey-answer2/survey-answer2.module.ts
+++ server/src/modules/survey-answer2/survey-answer2.module.ts
@@ -3,9 +3,15 @@ import {SurveyAnswer2Service} from './survey-answer2.service';
 import {SurveyAnswer2Controller} from './survey-answer2.controller';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {SurveyAnswer} from './entity/survey-answer.entity';
+import {User2Module} from '../user2/user2.module';
+import {Survey2Module} from '../survey2/survey2.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SurveyAnswer])],
+  imports: [
+    TypeOrmModule.forFeature([SurveyAnswer]),
+    User2Module,
+    Survey2Module,
+  ],
   providers: [SurveyAnswer2Service],
   controllers: [SurveyAnswer2Controller],
 })
